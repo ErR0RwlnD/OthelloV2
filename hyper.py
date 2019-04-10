663853aac5726ab331542a544aa1e2d8095a3a4e
ErR0RwlnD <yjz980208@gmail.com>
2019-04-10 08:54:07 +0800

    hypers

---
+++ hyper.py
@@ -0,0 +1,26 @@
+import os
+import torch
+
+
+class Hyper():
+
+    # board
+    black = 1
+    white = -1
+
+    # desnet wrapper
+    dropout = 0.3
+    epochs = 20
+    batch_size = 1800
+    cuda = torch.cuda.is_available()
+
+    # MCTS
+    sims = 40
+    cpuct = 1
+
+    # training
+    checkpoints = './checkpoints'
+    examples = './trainingExamples'
+
+    # examples generator
+    processing = 4
