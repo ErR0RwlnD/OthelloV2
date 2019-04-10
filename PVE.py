b2f410719a891d30cca69dff090632d2264a5948
ErR0RwlnD <yjz980208@gmail.com>
2019-04-10 08:49:39 +0800

    densenet ai VS input

---
+++ PVE.py
@@ -0,0 +1,22 @@
+from arena import Arena
+from mcts import MCTS
+from game import OthelloGame, display
+from utils import dotdict
+from player import *
+from wrapper import DensnetWrapper
+import numpy as np
+
+if __name__ == "__main__":
+    g = OthelloGame()
+    hp = HumanOthelloPlayer(g).play
+
+    net = DensnetWrapper(g)
+    net.load_checkpoint('overtraining_0.pth')
+    args = dotdict({'numMCTSSims': 500, 'cpuct': 1.0})
+    mcts = MCTS(g, net)
+
+    def netp(x):
+        return np.argmax(mcts.getAction(x, temp=0))
+
+    arena = Arena(netp, hp, g, display=display)
+    print(arena.playGames(2, verbose=True))
