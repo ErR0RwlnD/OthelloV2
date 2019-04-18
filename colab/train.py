23b06b9b0e6919231be486d871bac3ec47ed60eb
ErR0RwlnD <yjz980208@gmail.com>
2019-04-18 20:32:41 +0800

    colab only

--- colab/train.py
+++ colab/train.py
@@ -4,16 +4,16 @@ from game import OthelloGame
 from utils import dotdict, googleDrive
 
 args = dotdict({
-    'numIters': 1000,
+    'numIters': 800,
     'numEps': 100,
     'tempThreshold': 15,
-    'updateThreshold': 0.6,
+    'updateThreshold': 0.7,
     'maxlenOfQueue': 100000,
     'arenaCompare': 60,
 
     'load_model': True,
-    'checkpoint_file': 'best-7.pth',
-    'load_exmaples': True,
+    'checkpoint_file': 'day1-best.pth',
+    'load_exmaples': False,
     'examples_file': 'checkpoint_6.pth.examples',
     'numItersForTrainExamplesHistory': 20,
 })
