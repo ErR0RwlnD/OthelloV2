0248a8109e028fe040c2a5e5ecd5e25cb6f17dd1
ErR0RwlnD <yjz980208@gmail.com>
2019-05-12 19:52:46 +0800

    last commit

--- project1.py
+++ project1.py
@@ -1,8 +1,8 @@
-from arena import Arena
-from mcts import MCTS
-from game import OthelloGame, display
-from player import HumanOthelloPlayer
-from wrapper import DensenetWrapper
+from OthelloV2.arena import Arena
+from OthelloV2.mcts import MCTS
+from OthelloV2.game import OthelloGame, display
+from OthelloV2.player import HumanOthelloPlayer
+from OthelloV2.wrapper import DensenetWrapper
 import numpy as np
 import sys
 
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     hp = HumanOthelloPlayer(g).play
 
     net = DensenetWrapper(g)
-    net.load_checkpoint('best.pth')
+    net.load_checkpoint('bestc5.pth')
     mcts = MCTS(g, net)
 
     def netp(x):
@@ -21,11 +21,11 @@ if __name__ == "__main__":
     if str(choise) == '1':
         arena = Arena(netp, hp, g, display=display,
                       show_time=True, AI_first=True)
-        print(arena.playGames(1, verbose=True))
+        print(arena.playGames(1, verbose=True, bo1=True))
     elif str(choise) == '2':
         arena = Arena(hp, netp, g, display=display,
                       show_time=True, AI_first=False)
-        print(arena.playGames(1, verbose=True))
+        print(arena.playGames(1, verbose=True, bo1=True))
     else:
         print('指令无法接受')
         sys.exit(1)
